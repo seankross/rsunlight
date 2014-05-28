9bb0b4dc522a0d5607f1fb390c6170962266fe80
seankross <smk240@gmail.com>
2014-05-28 22:41:27 +0200

    changed getcommitteesall

--- R/cg_getcommitteesallleg.R
+++ R/cg_getcommitteesallleg.R
@@ -12,7 +12,7 @@ cg_getcommitteesallleg <- function(bioguide_id = NULL,
     key=getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")),
     callopts = list()) 
 {
-  url = "http://services.sunlightlabs.com/api/committees.allForLegislator.json"
+  url = "https://congress.api.sunlightfoundation.com/committees"
   args <- suncompact(list(apikey = key, bioguide_id = bioguide_id))
   tt <- GET(url, query=args, callopts)
   stop_for_status(tt)
